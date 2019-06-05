2ed2b8f5c0110b4e79642fb4ef88344a55a68a95
john2796 <jbmiranda22796@gmail.com>
2019-06-04 19:53:24 -0700

    completed

--- arrays/arrays.c
+++ arrays/arrays.c
@@ -28,8 +28,10 @@ Array *create_array(int capacity)
   arr->count = 0;
 
   // Allocate memory for elements
-  arr->elements = malloc(capacity * sizeof(char *));
-  return arr;
+  char **element = malloc(capacity * sizeof(char *));
+  array->elements = elements;
+
+  return array;
 }
 
 /*****
@@ -39,6 +41,9 @@ void destroy_array(Array *arr)
 {
 
   // Free all elements
+  for (int i = 0; i < arr->capacity; i++){
+   free(arr->elements[i]); 
+  }
 
   // Free array
 }
@@ -51,12 +56,19 @@ void resize_array(Array *arr)
 {
 
   // Create a new element storage with double capacity
+  char **new_elements = malloc(sizeof(char *) * (arr->capacity * 2));
 
   // Copy elements into the new storage
+  for (int i = 0; i < arr->count; i++ ){
+   new_elements[i] = arr->elements[i]; 
+  }
 
   // Free the old elements array (but NOT the strings they point to)
+  free(arr->elements);
 
   // Update the elements and capacity to new values
+  arr->elements = new_elements;
+  arr->capacity = arr->capacity * 2;
 }
 
 /************************************
@@ -74,8 +86,13 @@ char *arr_read(Array *arr, int index)
 {
 
   // Throw an error if the index is greater or equal to than the current count
+  if(index >= (*arr).count){
+   prerror("Index Not Found"); 
+   return NULL;
+  }
 
   // Otherwise, return the element at the given index
+  return arr->elements[index];
 }
 
 /*****
@@ -87,14 +104,25 @@ void arr_insert(Array *arr, char *element, int index)
 {
 
   // Throw an error if the index is greater than the current count
+  if(index >= (*arr).count) {
+   perror("Index Not Found."); 
+  }
 
   // Resize the array if the number of elements is over capacity
-
+    if(arr->count > arr->capacity){
+      resize_array(arr); 
+    }
   // Move every element after the insert index to the right one position
+   int prev_count = arr->count; 
+   for(int i = prev_count; i > index; i--) {
+    arr->elements[i] = arr->elements[i - 1]; 
+   }
 
   // Copy the element (hint: use `strdup()`) and add it to the array
+    arr->elements[index] = strdup(element);
 
   // Increment count by 1
+    arr->count++;
 }
 
 /*****
@@ -104,11 +132,16 @@ void arr_append(Array *arr, char *element)
 {
 
   // Resize the array if the number of elements is over capacity
+    if(arr->count == arr->capacity){
+      resize_array(arr); 
+    }
   // or throw an error if resize isn't implemented yet.
 
   // Copy the element and add it to the end of the array
+      strcpy(arr->elements[arr->count], element);
 
   // Increment count by 1
+    arr->count++;
 }
 
 /*****
@@ -122,10 +155,25 @@ void arr_remove(Array *arr, char *element)
 
   // Search for the first occurence of the element and remove it.
   // Don't forget to free its memory!
-
+    int found -1;
+    for (int i = 0; i< arr->count; i++ ){
+      int string_match = strcmp(element, arr->elements[i]);
+      if(string_match == 0) {
+        found = i;
+        free(arr->element[i]);
+        break;
+      }
+      if(found == -1) {
+       perror("Item not found");
+       exit(1);
+      }
+    }
   // Shift over every element after the removed element to the left one position
-
+  for(int j = found; j < arr->count -1; j++){
+    arr->elements[j] = arr->elements[j + 1]; 
+  }
   // Decrement count by 1
+  arr->count--;
 }
 
 /*****
